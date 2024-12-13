25572aeadfdcda5af9029013180f3050f073de97
Omkakde <kakadeom123@gmail.com>
2024-12-13 16:33:53 +0530

    natural and self joins

--- query_10_Natural&Self_Join.sql
+++ query_10_Natural&Self_Join.sql
@@ -6,6 +6,12 @@
 --  where department_id IN (1,2,4,5)
 
 
+-- SELECT d.department_id, d.department_name, c.name
+-- FROM departments as d
+-- JOIN customers as c on d.department_id = c.customer_id
+-- where d.department_name LIKE "%a"
+
+	
 SELECT d.department_id, d.department_name, c.name
 FROM departments as d
 JOIN customers as c on d.department_id = c.customer_id
