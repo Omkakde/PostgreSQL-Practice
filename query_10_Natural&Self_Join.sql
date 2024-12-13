bbf455269a77489579ec448b61ff0cf645e26aa9
Omkakde <kakadeom123@gmail.com>
2024-12-13 16:27:07 +0530

    Natural & SELF joins

---
+++ query_10_Natural&Self_Join.sql
@@ -0,0 +1,11 @@
+-- SELECT * FROM departments
+-- NATURAL JOIN customers
+
+-- SELECT * FROM departments
+--  NATURAL JOIN customers
+--  where department_id IN (1,2,4,5)
+
+
+SELECT d.department_id, d.department_name, c.name
+FROM departments as d
+JOIN customers as c on d.department_id = c.customer_id
