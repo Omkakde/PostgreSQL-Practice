e4d4fddd3ce5d3cc8394d183985e602b9105e454
Omkakde <kakadeom123@gmail.com>
2024-12-13 11:27:20 +0530

    clasues

---
+++ query_8_Clauses.sql
@@ -0,0 +1,25 @@
+-- select * from person
+-- where salary BETWEEN 60000 AND 70000
+
+
+-- SELECT * FROM person
+-- ORDER BY id DESC
+
+
+
+-- SELECT * FROM person
+-- ORDER BY id ASC LIMIT 5
+
+-- SELECT * FROM person
+-- WHERE citys IN ('Mumbai','Chennai','Delhi')
+-- OFFSET 2
+
+
+-- select * from person
+-- where name LIKE '%a'
+
+
+
+select DISTINCT citys from person
+
+
